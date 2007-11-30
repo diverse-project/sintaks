24f1de6ad19e686f316109890724f00d921f81bb
Didier Vojtisek <didier.vojtisek@inria.fr>
2007-11-30 22:24:57 +0000

    allow to use syntax file stored in the plugin
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/sintaks/trunk@55 492dbb7d-f086-47f9-86fe-51c427911f6e

--- sintaks_projects/org.kermeta.sintaks.ui/src/org/kermeta/sintaks/ui/Text2EcoreWizard.java
+++ sintaks_projects/org.kermeta.sintaks.ui/src/org/kermeta/sintaks/ui/Text2EcoreWizard.java
@@ -1,3 +1,12 @@
+/*$Id: Text2EcoreWizard.java,v 1.2 2007-11-30 22:24:57 dvojtise Exp $
+* Project : org.kermeta.sintaks
+* File : 	Text2EcoreWizard.java
+* License : EPL
+* Copyright : IRISA / INRIA / Universite de Haute Alsace
+* ----------------------------------------------------------------------------
+* Creation date : 29 juin 07
+* Authors : 
+*/
 /**
  * 
  */
@@ -32,10 +41,15 @@ public class Text2EcoreWizard extends SintaksWizard {
 		Master m = new Master();
 
 		String workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
+		URI smURI;
+		if(outputPage.getSMdlText().startsWith("platform:/plugin")){
+			smURI = URI.createPlatformPluginURI(outputPage.getSMdlText().replace("platform:/plugin", ""),false);
+		}
+		else smURI = URI.createFileURI(workspacePath + outputPage.getSMdlFile().getFullPath().toString());
 		m.getModelFromText(
 			URI.createFileURI(workspacePath + inputFile.getFullPath().toString()),
 			URI.createFileURI(workspacePath + targetFile.getFullPath().toString()),
-			URI.createFileURI(workspacePath + outputPage.getSMdlFile().getFullPath().toString())
+			smURI
 		);
 	}
 	
